a1a6b9c5365ea57c65d425633eceb33e3ab21507
DG <leekoon@gmail.com>
2022-07-01 21:39:53 +0900

    이상한 코드를 추가해봅시다

---
+++ src2.py
@@ -0,0 +1,8 @@
+
+
+class Invalid_Class_Name:
+  def test() -> None:
+    return None
+
+  def InternationalizationTranslateMethod(internationalizationCode: str, InternationalizationMessage: str) -> str:
+      return internationalizationCode + InternationalizationMessage
